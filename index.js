1f88f43bf2d8406234d890cf2e52fad86ac08fe6
derrick b <dbozich1@gmail.com>
2018-09-05 07:50:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 // Write your code in this file!
 const currentUser = "Thom Yorke";
-const welcomeMessage = 'Welcome to Flatbook, '+ currentUser;
+const welcomeMessage = 'Welcome to Flatbook, '+ currentUser + "!";
